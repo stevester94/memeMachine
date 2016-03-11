dd2a2e0bfc1e92e5b720b958be59e216a7c61b48
Steven Mackey <stevestser94@gmail.com>
2016-03-10 19:43:50 -0800

    ITS WORKING

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-
 var express = require('express');
 var app = express();
+var fs = require("fs");
+
+function randomIntFromInterval(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function getRandomMemeName() {
+    dir = fs.readdirSync("public/memes/");
+    randomInt = randomIntFromInterval(0, dir.length - 1);
+    return "memes/" + dir[randomInt];
+}
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.sendFile("index.html");
+
+    res.sendFile("index.html");
 });
 
 app.get('/meme', function (req, res) {
@@ -13,9 +24,10 @@ app.get('/meme', function (req, res) {
 });
 
 app.get('/randomMeme', function (req, res) {
-    res.send("img.jpg");
+    res.send(getRandomMemeName());
 });
 
 app.listen(80, function () {
+    getRandomMemeName();
   console.log('Example app listening on port 80!');
 });
