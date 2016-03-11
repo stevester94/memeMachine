20963f0974fff678efda43b8f309c433dfa8032b
Steven Mackey <stevestser94@gmail.com>
2016-03-10 18:32:33 -0800

    initial

---
+++ server.js
@@ -0,0 +1,17 @@
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile("index.html");
+});
+
+app.get('/meme', function (req, res) {
+    res.sendFile("public/img.jpg");
+});
+
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
+});
